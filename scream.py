0c127316473a6735ede2df18cfdb395e3876ccfd
LapisRaider <angielimenqi7@gmail.com>
2022-01-08 05:26:09 +0800

    - change to use delta time for movement

--- scream.py
+++ scream.py
@@ -15,13 +15,15 @@ class Scream(System):
     def on_enter(self, pet):
         pet.screamStartTime = time.time()
         pet.screamTime = random.randrange(Scream.MIN_SCREAM_TIME, Scream.MAX_SCREAM_TIME)
-        pet.set_anim_state(PetAnimState.HEADPAT)
+        pet.set_anim_state(PetAnimState.ATTACK_LEFT)
 
     def action(self, pet):
         #check when to stop screaming
         if time.time() > pet.screamStartTime + pet.screamTime:
             pet.change_state(PetState.IDLE)
             return
+        
+        #maybe if u pet the cat a few times go to headpat
 
         #scream
         #playsound("sfx/cat_meow.mp3", block=False)

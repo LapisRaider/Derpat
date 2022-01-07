f132d8b4fc618d690877112f3ac5d4c19cdf487f
Keith <e0544450@u.nus.edu>
2022-01-08 07:35:40 +0800

    Debug stuff

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     headpat = Headpat(6, PetState.HEADPAT)
     catch_mouse = CatchMouse(0, PetState.CATCH_MOUSE)
     snatch_mouse = SnatchMouse(0, PetState.GOT_MOUSE)
-    open_window = OpenWindow(0, PetState.CREATE_WINDOW, windows)
+    open_window = OpenWindow(0, PetState.CREATE_WINDOW)
     move_window = MoveWindow(0,PetState.DRAG_WINDOW)
     scream = Scream(0,PetState.SCREAM)
     # notepad = Notepad(width=400, height=200)
@@ -70,3 +70,15 @@ if __name__ == "__main__":
         # Exit the application, can be changed.
         if keyboard.is_pressed("ctrl") and keyboard.is_pressed('alt') and keyboard.is_pressed('2') and keyboard.is_pressed('9') and keyboard.is_pressed('Y'):
             break
+
+        #For debug purposes
+        if keyboard.is_pressed("1"):
+            pet.change_state(PetState.STROLL)
+        if keyboard.is_pressed("2"):
+            pet.change_state(PetState.HEADPAT)
+        if keyboard.is_pressed("3"):
+            pet.change_state(PetState.CATCH_MOUSE)
+        if keyboard.is_pressed("4"):
+            pet.change_state(PetState.CREATE_WINDOW)
+        if keyboard.is_pressed("5"):
+            pet.change_state(PetState.DRAG_WINDOW)

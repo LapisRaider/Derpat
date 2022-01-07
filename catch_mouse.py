cb168521d28a7f04036d910b5ce8fa42f799658c
LapisRaider <angielimenqi7@gmail.com>
2022-01-08 02:34:37 +0800

    snatch and catch mouse animation is fixed now plus the mouse is at the proper position

--- catch_mouse.py
+++ catch_mouse.py
@@ -4,13 +4,16 @@ import random
 from system import System
 from mouseController import *
 from pet import PetState
+from pet import PetAnimState
 
 class CatchMouse(System):
-    FOLLOW_SPEED = 10
-    MOUSE_CATCH_OFFSET = 10 
+    FOLLOW_SPEED = 1
+    MOUSE_CATCH_OFFSET = 5
 
     MAX_FOLLOW_TIME_AMT = 5 # after a certain amt of time give up
-    MIN_FOLLOW_TIME_AMT = 2 
+    MIN_FOLLOW_TIME_AMT = 3
+
+    CATCH_OFFSET = Vector2(64,64)
 
     #to be init at the start
     def on_enter(self, pet):
@@ -20,24 +23,29 @@ class CatchMouse(System):
     #pet follows the mouse ard
     def follow_mouse(self, pet):
         mousePos = getMousePos()
-        dir = mousePos.__sub__(pet.pos)
+        dir = mousePos.__sub__(pet.pos.__add__(CatchMouse.CATCH_OFFSET))
         dir = dir.normalised()
         pet.translate(round(dir.x) * CatchMouse.FOLLOW_SPEED, round(dir.y) * CatchMouse.FOLLOW_SPEED)
 
+        if (dir.x < 0 and pet.get_anim_state() != PetAnimState.WALK_LEFT):
+            pet.set_anim_state(PetAnimState.WALK_LEFT)
+        elif (dir.x >= 0 and pet.get_anim_state() != PetAnimState.WALK_RIGHT):
+            pet.set_anim_state(PetAnimState.WALK_RIGHT)
+
     #check if pet close enough to grab the mouse
     def check_get_mouse(self, pet):
-        dir = getMousePos().__sub__(pet.pos)
+        dir = getMousePos().__sub__(pet.pos.__add__(CatchMouse.CATCH_OFFSET))
         return dir.length() < CatchMouse.MOUSE_CATCH_OFFSET
 
     # update snatching the mouse
     def action(self, pet):
         #give up chasing
         if time.time() > pet.followStartTime + pet.followAmt:
-            pet.curr_state = PetState.IDLE
+            pet.next_state = PetState.IDLE
             return
 
         self.follow_mouse(pet) #follow the mouse
 
         #caught the mouse
         if self.check_get_mouse(pet):
-            pet.curr_state = PetState.GOT_MOUSE
+            pet.next_state = PetState.GOT_MOUSE

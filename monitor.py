65e8207c34fcb6da42ec6cdd7e33829106947223
LapisRaider <angielimenqi7@gmail.com>
2022-01-07 21:00:34 +0800

    more monitor stuff to check if out of bounds in y or x axis

--- monitor.py
+++ monitor.py
@@ -2,6 +2,20 @@ from screeninfo import get_monitors
 
 #print(str(monitors)), debugging purpose
 
+
+leftPosX = 0
+rightPosX = 0
+
+def initMonitors():
+    for monitor in get_monitors():
+        if monitor.x < leftPosX:
+            leftPosX = monitor.x
+
+        if monitor.width + monitor.x > rightPosX:
+            rightPosX = monitor.width + monitor.x
+
+
+
 #get which monitor you're in depending on pos X, y is unimpt here
 def getMonitorOnScrPos(pos):
     for monitor in get_monitors():
@@ -10,3 +24,12 @@ def getMonitorOnScrPos(pos):
 
     return None
 
+#check if out of monitor hori axis
+def outOfRangeHori(pos):
+    return pos.x < leftPosX or pos.x > rightPosX
+
+#change if out of monitors vert axis
+def outOfRangeVert(pos):
+    currMonitor = getMonitorOnScrPos(pos)
+    return pos.y > currMonitor.y + currMonitor.height or pos.y < 0
+ 

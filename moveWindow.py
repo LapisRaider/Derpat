badfd6b3b7bd0c1ed267c8745b27199e3cdfdf02
Keith <e0544450@u.nus.edu>
2022-01-08 02:45:51 +0800

    Now able to move windows, I hope

--- moveWindow.py
+++ moveWindow.py
@@ -3,6 +3,20 @@ import time
 import random
 import win32gui
 
+from pet import PetAnimState, PetState
+from system import System
+from vector2 import Vector2
+
+import ctypes
+from ctypes import c_int
+import ctypes.wintypes
+from ctypes.wintypes import HWND, DWORD
+dwmapi = ctypes.WinDLL("dwmapi")
+DWMWA_CLOAKED = 14 
+isCloaked = c_int(0)
+
+import monitor
+
 ## For moving non-tkInter windows
 
 # Gets the current active window
@@ -12,30 +26,80 @@ def getActiveWindow():
 
 def translateWindow(window,x,y):
     # Only moves the window if it is in the foreground
-    if win32gui.GetForegroundWindow() == window:
+    #if win32gui.GetForegroundWindow() == window:
         rect = win32gui.GetWindowRect(window)
         win32gui.MoveWindow(window,rect[0]+x,rect[1]+y,rect[2]-rect[0],rect[3]-rect[1],True)
         #win32gui.SetWindowPos(window,0,0,0,rect[2],rect[3],0x0004)
         rect = win32gui.GetWindowRect(window)
         # For debugging
-        print(win32gui.GetWindowText(window))
-        print(rect)
+        #print(win32gui.GetWindowText(window))
+        #print(rect)
         return True
-    else:
-        return False
+    #else:
+    #    return False
 
 # Callback function for EnumWindows
 def windowEnumHandler(hwnd, list):
-    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '':
-        list.append(hwnd)
-        print("Window Name:", win32gui.GetWindowText(hwnd))
+    tempMonitor = monitor.getMonitorOnScrPos(Vector2(0,0)) # Used to get screen width/height
+
+    #Just tons of checks to make sure I don't get any invalid windows
+    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) != '' and win32gui.GetWindowText(hwnd) != 'tk' and win32gui.GetWindowText(hwnd) != 'Task Manager':
+        dwmapi.DwmGetWindowAttribute(HWND(hwnd), DWORD(DWMWA_CLOAKED), ctypes.byref(isCloaked), ctypes.sizeof(isCloaked))
+        if(isCloaked.value == 0): # Checking if window is suspended
+            rect = win32gui.GetWindowRect(hwnd)
+            if rect[0] > 0 and rect[0] < tempMonitor.height and rect[1] > 0 and rect[1] < tempMonitor.width: # If within window borders
+                if rect[2] - rect[0] > 0 and rect[3] - rect[1] > 0: # If size of window larger than 0
+                    list.append(hwnd)
+                    print("Window Name:", win32gui.GetWindowText(hwnd))
     return True
 
 def getRandomWindow():
     top_windows = []
     win32gui.EnumWindows(windowEnumHandler,top_windows)
     if len(top_windows) > 0:
-        randomNum = random.randint(0,len(top_windows))
+        randomNum = random.randint(0,len(top_windows)-1)
+        print("Window Found:",win32gui.GetWindowText(top_windows[randomNum]))
         return top_windows[randomNum]
     else:
         return None
+
+class MoveWindow(System):
+    DISTANCE_TO_TRAVEL = 500
+
+    def __init__(self, delay=0, action_state=PetState.DEFAULT):
+        # State 0 = moving to window, State 1 = moving window
+        self.state = 0
+        super().__init__(delay=delay, action_state=action_state)
+        
+    def on_enter(self, pet):
+        self.state = 0
+        self.targetWindow = getRandomWindow()
+        self.distanceTravelled = 0
+        rect = win32gui.GetWindowRect(self.targetWindow)
+        direction = Vector2(rect[0],rect[1]).__sub__(pet.pos)
+        if direction.x > 0:
+            pet.set_anim_state(PetAnimState.WALK_RIGHT)
+        else:
+            pet.set_anim_state(PetAnimState.WALK_LEFT)
+
+    def action(self,pet):
+        if self.state == 0:
+            rect = win32gui.GetWindowRect(self.targetWindow)
+            direction = Vector2(rect[0],rect[1]).__sub__(pet.pos)
+
+            if direction.length() < 1:
+                self.state = 1
+                pet.set_anim_state(PetAnimState.WALK_RIGHT)
+                win32gui.SetForegroundWindow(self.targetWindow)
+            else:
+                normal = direction.normalised()
+                pet.translate(round(normal.x),round(normal.y))
+            # If window loses focus, return to idle state
+            #if getActiveWindow() != self.targetWindow:
+            #    pet.change_state(PetState.IDLE)
+        elif self.state == 1:
+            pet.translate(1,0)
+            # If window loses focus, return tto idle state
+            self.distanceTravelled = self.distanceTravelled + 1
+            if not translateWindow(self.targetWindow,1,0) or self.distanceTravelled >= MoveWindow.DISTANCE_TO_TRAVEL:
+                pet.change_state(PetState.IDLE)
